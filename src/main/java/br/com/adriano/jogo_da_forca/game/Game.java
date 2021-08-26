49ce6cc5ee893a1beaab4cc5c218d018c9cf6300
Adriano-Ivan <adrianoivandepaulacampos@gmail.com.br>
2021-08-26 15:33:42 -0300

    Classe Word.

--- src/main/java/br/com/adriano/jogo_da_forca/game/Game.java
+++ src/main/java/br/com/adriano/jogo_da_forca/game/Game.java
@@ -1,8 +1,15 @@
 package br.com.adriano.jogo_da_forca.game;
 
+import br.com.adriano.jogo_da_forca.core.Word;
+
 public class Game {
 
 	public void start() {
-		
+		Word word = new Word("Casa");
+		word.hasChar('a');
+		word.hasChar('c');
+		word.hasChar('s');
+		System.out.println(word.discovered());
+		System.out.println(word.toString());
 	}
 }
